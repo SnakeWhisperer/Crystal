aa12db296ac1e386ca5d49430f931185a07aa505
SnakeWhisperer <snake@whisperer.com>
2025-06-10 17:29:12 -0400

    RS485 comm established with Tera Term

--- entrance/entrance.ino
+++ entrance/entrance.ino
@@ -1,12 +1,16 @@
 void setup() {
-    Serial.begin(115200);
-    Serial2.begin(19200, SERIAL_8N1);
-    Serial.println("Waiting for barcode scanner input...");
+    Serial.begin(115200);                                       // Debug to Serial Monitor
+    Serial2.begin(19200, SERIAL_8N1);                           // Scanner
+    Serial3.begin(9600);                                        // RS485 (via MAX485)
+    Serial.println("System ready...");
 }
 
 void loop() {
     while (Serial2.available()) {  // Read data immediately
         char data = Serial2.read();
         Serial.write(data);  // Print character as it arrives
+
+
+        Serial3.write(data);            // Send via RS485 to PC
     }
 }
